d2577d28aa83d37d44391b3080833ced31c8807e
Ricky Ariansyah <rickyarians@outlook.com>
2019-05-02 03:14:32 +0700

    Update Soal4.php

--- Soal4.php
+++ Soal4.php
@@ -1,24 +1,19 @@
 <?php
-
-
-
 $datalain = [
     ['g','h','i','j'],
     ['a','c','b','e','d'],
     ['g','e','f']
 ];
-
-
 function sortarray($datalain)
 {
+    for($i = 0; $i < count($datalain); $i++)
+    {
+        sort($datalain[$i]);
+    }
     array_multisort($datalain);
     print_r($datalain);
-
-
 }
 
 
 sortarray($datalain);
-
-
 ?>
